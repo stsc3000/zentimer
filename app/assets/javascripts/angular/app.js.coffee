8e2acb4aa278d1e448478f8d8bc3474a4909d208
stsc3000 <steffen.schildknecht@zweitag.de>
2014-03-03 09:00:55 +0100

    new done and delete buttons

--- app/assets/javascripts/angular/app.js.coffee
+++ app/assets/javascripts/angular/app.js.coffee
@@ -2,7 +2,7 @@
 #= require_tree .
 
 
-angular.module("app", ["zen-timer", "fitText", "users", "entries", "settings", "ngAnimate", "ngRoute"])
+angular.module("app", ["zen-timer", "ngFitText", "users", "entries", "settings", "ngAnimate", "ngRoute"])
 
 angular.element(document).ready ->
   if $("meta[name=user-token]").length > 0
--- vendor/assets/javascripts/angularjs-fittext.js
+++ vendor/assets/javascripts/angularjs-fittext.js
@@ -1,55 +1,51 @@
-var module = angular.module('fitText', []);
+/* ng-FitText.js v2.0.0
+ * https://github.com/patrickmarabeas/ng-FitText.js
+ *
+ * Original jQuery project: https://github.com/davatron5000/FitText.js
+ *
+ * Copyright 2014, Patrick Marabeas http://marabeas.io
+ * Released under the MIT license
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * Date: 25/02/2014
+ */
 
-module.directive('fittext', [ '$window', '$document', function( $window, $document ) {
-  return {
-    restrict: 'A',
-    scope: {
-      watch: '=',
-      compressor: '@'
-    },
-    link: function( $scope, $element, $attrs ) {
+'use strict';
 
-      $scope.compressor = 0.85;
-      $scope.minFontSize = $attrs.min || Number.NEGATIVE_INFINITY;
-      $scope.maxFontSize = $attrs.max || Number.POSITIVE_INFINITY;
+angular.module( 'ngFitText', [] )
+	.directive( 'fittext', [ function() {
+		return {
+			restrict: 'A',
+			scope: true,
+			transclude: true,
+			replace: true,
+			template: function( element, attrs ) {
+				var tag = element[0].nodeName;
+				return "<"+tag+" data-ng-transclude data-ng-style='{fontSize:fontSize}'></"+tag+">";
+			},
+			link: function( scope, element, attrs ) {
+				scope.compressor = attrs.fittext || 1;
+				scope.minFontSize = attrs.fittextMin || Number.NEGATIVE_INFINITY;
+				scope.maxFontSize = attrs.fittextMax || Number.POSITIVE_INFINITY;
+				scope.elementWidth = element[0].offsetWidth;
 
-      var resizer = function() {
+				( scope.resizer = function() {
+					scope.fontSize = Math.max(
+						Math.min(
+							scope.elementWidth / ( scope.compressor * 10 ),
+							parseFloat( scope.maxFontSize )
+						),
+						parseFloat( scope.minFontSize )
+					) + 'px';
 
-        console.log("resizer", $scope.compressor);
-        $scope.fontSize = Math.max(
-          Math.min(
-            $element[0].offsetWidth / ( $scope.compressor * 10 ),
-            parseFloat( $scope.maxFontSize )
-        ),
-        parseFloat( $scope.minFontSize )
-        ) + 'px';
-        console.log($element[0].offsetWidth);
-        console.log($scope.fontSize);
-        $element.css('font-size', $scope.fontSize);
-      };
+					if( !scope.$$phase ) scope.$digest();
 
-      $scope.$watch('watch', function(newValue, oldValue) {
-        $scope.compressor = $attrs.compressor || 1;
-        $scope.minFontSize = $attrs.min || Number.NEGATIVE_INFINITY;
-        $scope.maxFontSize = $attrs.max || Number.POSITIVE_INFINITY;
+				})();
 
-
-        var newValueIsHours = parseInt(newValue) >= 3600;
-        var oldValueIsHours = parseInt(oldValue) >= 3600;
-
-
-        if (newValueIsHours !== oldValueIsHours) { 
-          $scope.compressor = 0.65;
-          resizer();
-        }
-
-        angular.element( $window ).bind( 'resize', function() {
-          resizer();
-        });
-      });
-
-      resizer();
-
-    }
-  };
-}]);
+				angular.element( window ).bind( 'resize', function() {
+					scope.elementWidth = element[0].offsetWidth;
+					scope.resizer();
+				});
+			}
+		}
+	}]);
