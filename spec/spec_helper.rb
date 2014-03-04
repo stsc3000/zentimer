d7dbc03631318831b261fb53ffb5f0943fec016d
stsc3000 <steffen.schildknecht@zweitag.de>
2014-03-04 18:22:44 +0100

    Add spec focusing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,6 +24,12 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  #Filtering specs
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
