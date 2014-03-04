3c571d35beadc95cf182ed413a4f9e6cb76a3484
stsc3000 <steffen.schildknecht@zweitag.de>
2014-03-04 07:35:52 +0100

    Add entry scopes for dates for dates

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,9 @@
 class Entry < ActiveRecord::Base
   belongs_to :user, inverse_of: :entries
   acts_as_ordered_taggable_on :tags
+
+  scope :today, -> { between Time.now.beginning_of_day, Time.now.end_of_day }
+  scope :this_week, -> { between Time.now.beginning_of_week, Time.now.end_of_week }
+  scope :this_month, -> { between Time.now.beginning_of_month, Time.now.end_of_month  }
+  scope :between, ->(from, to) { where("updated_at >= ?", from).where("updated_at <= ?", to) }
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,5 +1,70 @@
 require 'spec_helper'
 
 describe Entry do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "fetches entries from today" do
+    today_entry = Entry.create
+
+    tomorrow_entry = Entry.create.tap do |entry|
+      entry.update_column :updated_at, (Time.now + 1.day).beginning_of_day
+    end
+
+    yesterday_entry = Entry.create.tap do |entry|
+      entry.update_column :updated_at, (Time.now - 1.day).beginning_of_day
+    end
+
+    expect(Entry.today).to include(today_entry)
+    expect(Entry.today).not_to include(tomorrow_entry)
+    expect(Entry.today).not_to include(yesterday_entry)
+  end
+
+  it "fetches entries from this week" do
+    this_week_entry = Entry.create
+
+    next_week_entry = Entry.create.tap do |entry|
+      entry.update_column :updated_at, (Time.now + 7.day).beginning_of_day
+    end
+
+    last_week_entry = Entry.create.tap do |entry|
+      entry.update_column :updated_at, (Time.now - 7.day).beginning_of_day
+    end
+
+    expect(Entry.this_week).to include(this_week_entry)
+    expect(Entry.this_week).not_to include(next_week_entry)
+    expect(Entry.this_week).not_to include(last_week_entry)
+  end
+
+  it "fetches entries from this month" do
+    today_entry = Entry.create
+
+    next_month_entry = Entry.create.tap do |entry|
+      end_of_month = Time.now.end_of_month + 1.day
+      entry.update_column :updated_at, end_of_month
+    end
+
+    last_month_entry = Entry.create.tap do |entry|
+      start_of_month = Time.now.beginning_of_month - 1.day
+      entry.update_column :updated_at, start_of_month
+    end
+
+    expect(Entry.this_month).to include(today_entry)
+    expect(Entry.this_month).not_to include(next_month_entry)
+    expect(Entry.this_month).not_to include(last_month_entry)
+  end
+
+  it "fetches arbitrary start and end dates" do
+    today_entry = Entry.create
+
+    in_two_days_entry = Entry.create.tap do |entry|
+      entry.update_column :updated_at, Time.now + 2.days
+    end
+
+    two_days_ago_entry = Entry.create.tap do |entry|
+      entry.update_column :updated_at, Time.now - 2.days
+    end
+
+    entries = Entry.between(Time.now - 1.days, Time.now + 1.days)
+    expect(entries).to include(today_entry)
+    expect(entries).not_to include(in_two_days_entry)
+    expect(entries).not_to include(two_days_ago_entry)
+  end
 end
