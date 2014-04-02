5a50657e7a4badd7567bbc377a8741eaca38b500
stsc3000 <steffen.schildknecht@zweitag.de>
2014-04-02 07:29:43 +0200

    Add logged_at for analytics view

--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -1,6 +1,6 @@
 class EntrySerializer < ActiveModel::Serializer
   attributes :id, :elapsed, :lastTick, :tag_list, :running,
-    :current, :project
+    :current, :project, :logged_at
 
   def lastTick
     object.lastTick.iso8601 if object.lastTick
@@ -8,6 +8,10 @@ class EntrySerializer < ActiveModel::Serializer
 
   def tag_list
     object.tags.map(&:name)
+  end
+
+  def logged_at
+    object.updated_at.to_date
   end
 
 end
